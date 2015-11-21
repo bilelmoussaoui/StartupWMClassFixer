aa174ef1b9b3dfa1391483ddf57dbdbb0e06d35e
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2015-11-21 12:30:44 +0100

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -4,26 +4,38 @@
 # Version : 0.1 beta
 # Licence : The script is released under GPL
 
-applications_location=("/usr/share/applications/" "/usr/local/share/applications/" "/usr/local/share/applications/kde4" "/home/${SUDO_USER:-$USER}/.local/share/applications/" "/home/${SUDO_USER:-$USER}/.local/share/applications/kde4/" $(xdg-user-dir DESKTOP))
+applications_location=("/usr/share/applications/"
+									"/usr/local/share/applications/"
+									"/usr/local/share/applications/kde4"
+									"/home/${SUDO_USER:-$USER}/.local/share/applications/"
+									"/home/${SUDO_USER:-$USER}/.local/share/applications/kde4/"
+									$(xdg-user-dir DESKTOP))
 IFS=,
 
-#The script needs root privileges 
+#The script needs root privileges
 if [ "$(id -u)" != "0" ]; then
-	echo "ou need to have root privileges to run the script.Please try again, this time using 'sudo'. Exiting."
+  echo "You need to have root privileges to run the script."
+  echo "Please try again,this time using 'sudo'. Exiting."
+  exit 1
+fi
+
+if [ ! -f "database.csv" ]; then
+	echo "Please download the database file first! Exiting."
 	exit 1
 fi
+
 {
-	read;
-	while read -r name launcher startupwmclass; do
-	    for app_location in "${applications_location[@]}"
-	    do  
-	    	desktop_file="$app_location$launcher.desktop"
-	        if [ -f "$desktop_file" ]; then
-                if ! grep -Gq "StartupWMClass\s*=\s*$startupwmclass$" "$desktop_file"; then
-	        		echo "StartupWMClass = $startupwmclass" >> "$desktop_file"	
-	        		echo "Fixed : $name"
-	        	fi
-	        fi
-	    done
-	done 
+  read;
+  while read -r name launcher startupwmclass; do
+      for app_location in "${applications_location[@]}"
+      do
+        desktop_file="$app_location$launcher.desktop"
+        if [ -f "$desktop_file" ]; then
+          if ! grep -Gq "StartupWMClass\s*=\s*$startupwmclass$" "$desktop_file"; then
+            echo "StartupWMClass = $startupwmclass" >> "$desktop_file"
+            echo "Fixed : $name"
+          fi
+        fi
+      done
+  done
 }< "database.csv"
